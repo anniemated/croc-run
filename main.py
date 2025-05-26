0c77b9c94d93f825a6ba7a89cee81ba9151d6cf9
Annie Ma <annie9ma@gmail.com>
2025-05-26 13:13:18 -0400

    basic formatting changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Dino Game in Python
 
 A game similar to the famous Chrome Dino Game, built using pygame-ce.
-Made by intern: @bassemfarid, no one or nothing else. 🤖
+Made by intern: Annie!!
 """
 
 import pygame
@@ -13,7 +13,7 @@ clock = pygame.time.Clock()
 running = True  # Pygame main loop, kills pygame when False
 
 # Game state variables
-is_playing = True  # Whether in game or in menu
+screen_type = 1 # 1: menu, 2: game, 3: game over
 GROUND_Y = 300  # The Y-coordinate of the ground level
 JUMP_GRAVITY_START_SPEED = -20  # The speed at which the player jumps
 players_gravity_speed = 0  # The current speed at which the player falls
@@ -31,15 +31,37 @@ player_rect = player_surf.get_rect(bottomleft=(25, GROUND_Y))
 egg_surf = pygame.image.load("graphics/egg/egg_1.png").convert_alpha()
 egg_rect = egg_surf.get_rect(bottomleft=(800, GROUND_Y))
 
+def menu():
+    screen.fill("white")
+
+def game():
+    screen.fill("purple")  # Wipe the screen
+
+    # Blit the level assets
+    screen.blit(SKY_SURF, (0, 0))
+    screen.blit(GROUND_SURF, (0, GROUND_Y))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+    screen.blit(score_surf, score_rect)
+
+    # Adjust egg's horizontal location then blit it
+    egg_rect.x -= 5
+    if egg_rect.right <= 0:
+        egg_rect.left = 800
+    screen.blit(egg_surf, egg_rect)
+
+def game_over():
+    screen.fill("black")
 
 while running:
-    # Poll for events
+# Player actions
     for event in pygame.event.get():
         # pygame.QUIT --> user clicked X to close your window
         if event.type == pygame.QUIT:
             running = False
 
-        elif is_playing:
+        # Player movement
+        elif screen_type == 2:
             # When player wants to jump by pressing SPACE
             if (
                 event.type == pygame.KEYDOWN
@@ -47,42 +69,34 @@ while running:
                 or event.type == pygame.MOUSEBUTTONDOWN
             ) and player_rect.bottom >= GROUND_Y:
                 players_gravity_speed = JUMP_GRAVITY_START_SPEED
+
         else:
             # When player wants to play again by pressing SPACE
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                is_playing = True
+                screen_type = 2
                 egg_rect.left = 800
 
-    if is_playing:
-        screen.fill("purple")  # Wipe the screen
+# Different screens
+    # Menu screen
+    if screen_type == 1:
+        menu()
 
-        # Blit the level assets
-        screen.blit(SKY_SURF, (0, 0))
-        screen.blit(GROUND_SURF, (0, GROUND_Y))
-        pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-        screen.blit(score_surf, score_rect)
-
-        # Adjust egg's horizontal location then blit it
-        egg_rect.x -= 5
-        if egg_rect.right <= 0:
-            egg_rect.left = 800
-        screen.blit(egg_surf, egg_rect)
-
+    # Game screen
+    elif screen_type == 2:
+        game()
         # Adjust player's vertical location then blit it
         players_gravity_speed += 1
         player_rect.y += players_gravity_speed
         if player_rect.bottom > GROUND_Y:
             player_rect.bottom = GROUND_Y
         screen.blit(player_surf, player_rect)
-
         # When player collides with enemy, game ends
         if egg_rect.colliderect(player_rect):
-            is_playing = False
-
-    # When game is over, display game over message
-    else:
-        screen.fill("black")
+            screen_type = 3
+        
+    # Game over screen
+    elif screen_type == 3:
+        game_over()
 
     # flip the display to put your work on screen
     pygame.display.flip()
